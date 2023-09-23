9bee37e17dfef0ab426f1b65d3aa4fd03bf0cd77
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-23 23:21:00 +0600

    add mapping

--- first_project/src/App.js
+++ first_project/src/App.js
@@ -2,28 +2,35 @@
 import './App.css';
 
 function App() {
+  const nayoks = ['sakib', 'kobir', 'omuk', 'tomuk', 'taimur']
   return (
     <div className="App">
-      {/* call kora hoyese */}
-      <Person title="REACT"></Person>
-      <Person></Person>
-      <Person></Person>
-     
+
+      {
+        nayoks.map(nayok => 
+          <Person  name= {nayok}></Person>
+        )
+      }
+       {/* call kora hoyese  */}
+       {/* <Person title="REACT"></Person> */}
+
     </div>
   );
 }
 // add new component create 
-function Person(props){
+function Person(props) {
+  
   console.log('the props is:', props);
-  return(
+  return (
     <div className='conatiner'>
-       <h1 >hello react app with data passing or called props IN {props.title}</h1>
+      <h1>nayok name is {props.name} </h1>
+      <h1 >hello react app with data passing or called props IN {props.title}</h1>
        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, tempora.</p>
        <h3>logic appliy </h3>
-       
+
     </div>
-    
-   
+
+
   )
 }
 export default App;
