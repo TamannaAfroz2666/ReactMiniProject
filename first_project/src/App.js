a25adb7cb3a094d3c67232ef1a8f24d29f505882
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-23 20:20:18 +0600

    using props

--- first_project/src/App.js
+++ first_project/src/App.js
@@ -5,15 +5,25 @@ function App() {
   return (
     <div className="App">
       {/* call kora hoyese */}
+      <Person title="REACT"></Person>
+      <Person></Person>
       <Person></Person>
      
     </div>
   );
 }
 // add new component create 
-function Person(){
+function Person(props){
+  console.log('the props is:', props);
   return(
-    <h1>hello react app</h1>
+    <div className='conatiner'>
+       <h1 >hello react app with data passing or called props IN {props.title}</h1>
+       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, tempora.</p>
+       <h3>logic appliy </h3>
+       
+    </div>
+    
+   
   )
 }
 export default App;
