acb01162a253ec0409a384d84d0826e2e790ea9d
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-24 23:40:09 +0600

    add creating useEffects

--- first_project/src/App.js
+++ first_project/src/App.js
@@ -2,32 +2,46 @@
 import './App.css';
 
 function App() {
-  const nayoks = ['sakib', 'kobir', 'omuk', 'tomuk', 'taimur']
+  // const nayoks = ['sakib', 'kobir', 'omuk', 'tomuk', 'taimur']
   return (
     <div className="App">
 
-      {
+      {/* {
         nayoks.map(nayok => 
           <Person  name= {nayok}></Person>
         )
-      }
+      } */}
     </div>
   );
 }
-// add new component create 
-function Person(props) {
-  
-  console.log('the props is:', props);
-  return (
-    <div className='conatiner'>
-      <h1>nayok name is {props.name} </h1>
-      <h1 >hello react app with data passing or called props IN {props.title}</h1>
-       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, tempora.</p>
-       <h3>logic appliy </h3>
 
+// add new component 
+
+function ExternalUsers(){
+  return(
+    <div>
+      <h1>External users</h1>
     </div>
-
-
   )
 }
+
+
+
+
+// add new component create 
+// function Person(props) {
+  
+//   console.log('the props is:', props);
+//   return (
+//     <div className='conatiner'>
+//       <h1>nayok name is {props.name} </h1>
+//       <h1 >hello react app with data passing or called props IN {props.title}</h1>
+//        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore, tempora.</p>
+//        <h3>logic appliy </h3>
+
+//     </div>
+
+
+//   )
+// }
 export default App;
