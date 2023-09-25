5f64446d3a2af394da36acae0cfb9d59a5b75bac
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-25 20:58:24 +0600

    add styled

--- first_project/src/components/Cosmetic/Cosmetic.js
+++ first_project/src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
-    const {} = props.CosmeticData;
+    const {id, name, balance} = props.CosmeticData;
     return (
         <div className='product'>
 
-            <h2>product name:</h2>
-            <h2>product price:</h2>
-            <h2>product id : </h2>
+            <h2>product name:{name}</h2>
+            <h2>product price: {balance}</h2>
+            <h2>product id : {id}</h2>
+            <button type='button'>Add to cart</button>
         </div>
     );
 };
