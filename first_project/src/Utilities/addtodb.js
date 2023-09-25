cf73d7a215671072a8c7a986df93c6dfe5e7b4d6
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-25 23:37:22 +0600

    add object as jons in localstroge

--- first_project/src/Utilities/addtodb.js
+++ first_project/src/Utilities/addtodb.js
@@ -2,19 +2,36 @@
 
 const addToDb = (id) =>{
 
-    let shoppingCart = {};
-    localStorage.setItem('Shopping-cart',JSON.stringify(shoppingCart));
-    const quantity = localStorage.getItem(id);
+    let shoppingCart;
+    
+    // get the shopping cart 
+    const storedCart = localStorage.getItem('Shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
+        console.log('shoppingCart',shoppingCart);
+    }
+    else{
+        shoppingCart = {};
+    }
+
+
+
+    // add quantity 
+    const quantity = shoppingCart[id];
     if(quantity){
-        const newQuantity = parseInt(quantity) +1;
+        const newQuantity = quantity +1;
+        shoppingCart[id] = newQuantity;
+
         // localStorage.setItem(id, newQuantity)
 
     }
     else{
-        localStorage.setItem(id, 1);
+        shoppingCart[id] = 1;
+        // localStorage.setItem(id, 1);
 
     }
-
+    // nichey cz jkono item add korte hobe new or old 
+    localStorage.setItem('Shopping-cart',JSON.stringify(shoppingCart));
 
 
    
