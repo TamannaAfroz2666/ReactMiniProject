3aa4d8781f8e21536322ad9655a1cc55ac8e18d5
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-25 23:21:03 +0600

    use jons stringify method

--- first_project/src/Utilities/addtodb.js
+++ first_project/src/Utilities/addtodb.js
@@ -1,7 +1,23 @@
 // use local storage to manage cart data 
 
-const addToDb = id =>{
-    localStorage.setItem(id, 1)
+const addToDb = (id) =>{
+
+    let shoppingCart = {};
+    localStorage.setItem('Shopping-cart',JSON.stringify(shoppingCart));
+    const quantity = localStorage.getItem(id);
+    if(quantity){
+        const newQuantity = parseInt(quantity) +1;
+        // localStorage.setItem(id, newQuantity)
+
+    }
+    else{
+        localStorage.setItem(id, 1);
+
+    }
+
+
+
+   
 
 }
 export {addToDb}
