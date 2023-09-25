35b95772edde6dd284a231dbc5e8871e9a3eaab9
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-25 14:21:45 +0600

    add useEffect

--- first_project/src/components/ShowCountry/Countries.js
+++ first_project/src/components/ShowCountry/Countries.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Countries.css'
 
 const Countries = () => {
+    const [countries, setCountries] = useState([])
+
+    useEffect(()=>{
+        fetch(`https://restcountries.com/v3.1/all`)
+        .then(res =>res.json())
+        .then(data =>{
+            console.log('the country data is :', data);
+            setCountries(data);
+
+        })
+
+    },[])
     return (
         <div>
-            <h2>show the country list</h2>
+            <h2>show the country list Lenght {countries.length}</h2>
         </div>
     );
 };
