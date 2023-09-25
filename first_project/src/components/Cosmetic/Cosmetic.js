088ba32bb0eee9aa3711dc02941ad0c8f4c1b268
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-25 21:15:41 +0600

    get id frow api

--- first_project/src/components/Cosmetic/Cosmetic.js
+++ first_project/src/components/Cosmetic/Cosmetic.js
@@ -5,9 +5,14 @@ const Cosmetic = (props) => {
     const {id, name, balance} = props.CosmeticData;
 
     // event handler work 
-    const addCard = () =>{
+    const addCard = (id) =>{
         console.log('hello shopping');
+        console.log('product id is', id);
 
+    } 
+
+    const addToCard = () =>{
+    addCard(id)
     }
     return (
         <div className='product'>
@@ -15,7 +20,8 @@ const Cosmetic = (props) => {
             <h2>product name:{name}</h2>
             <h2>product price: {balance}</h2>
             <h2>product id : {id}</h2>
-            <button type='button' onClick={addCard}>Add to cart</button>
+            <button type='button' onClick={addToCard}>Add to cart</button>
+            <button type='button' onClick={()=>addCard(id)}>Add to cart sortcut</button>
         </div>
     );
 };
