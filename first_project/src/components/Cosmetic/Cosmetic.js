5ed3d7dd33430b7268c8600dfce7fd20dd948ffc
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-25 21:02:38 +0600

    event handler added

--- first_project/src/components/Cosmetic/Cosmetic.js
+++ first_project/src/components/Cosmetic/Cosmetic.js
@@ -3,13 +3,19 @@ import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     const {id, name, balance} = props.CosmeticData;
+
+    // event handler work 
+    const addCard = () =>{
+        console.log('hello shopping');
+
+    }
     return (
         <div className='product'>
 
             <h2>product name:{name}</h2>
             <h2>product price: {balance}</h2>
             <h2>product id : {id}</h2>
-            <button type='button'>Add to cart</button>
+            <button type='button' onClick={addCard}>Add to cart</button>
         </div>
     );
 };
