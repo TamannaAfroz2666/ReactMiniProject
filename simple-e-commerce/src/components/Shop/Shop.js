162c8c0875435f09c8e3455ac1a9ae1352ef8ad4
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-26 13:36:54 +0600

    add componenet add

---
+++ simple-e-commerce/src/components/Shop/Shop.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Shop.css';
+
+const Shop = () => {
+    return (
+        <div className='shop-container'>
+            <div className="shopping-container">
+                <h1>ghg</h1>
+            </div>
+            <div className="card-container">
+                <h2>hgfdd</h2>
+
+            </div>
+           
+        </div>
+    );
+};
+
+export default Shop;
