a4f752798039c891ae46e8e9d266dc33314450b7
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-26 11:30:18 +0600

    add component and connect

--- simple-e-commerce/src/App.js
+++ simple-e-commerce/src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>ggf</h1>
+    <Header></Header>
       
     </div>
   );
