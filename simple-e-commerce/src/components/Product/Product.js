33e1d262cc71b924f04ab70df9b75721b3e384c4
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-26 14:58:35 +0600

    used botton

--- simple-e-commerce/src/components/Product/Product.js
+++ simple-e-commerce/src/components/Product/Product.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import './Product.css'
 const Product = (props) => {
-   const {name, img, price, seller, ratings} = props.products;
+    const { name, img, price, seller, ratings } = props.products;
     return (
         <div className='product'>
-          <img src={img} alt="" />
-          <div className="product-info">
-          <p className='product-name'>{name}</p>
-          <p> price {price}</p>
-          <p><small>Seller {seller}</small></p>
-          <p><small>Ratings {ratings}</small></p>
-          </div>
-          
-            
+            <img src={img} alt="" />
+            <div className="product-info">
+                <p className='product-name'>{name}</p>
+                <p> price {price}</p>
+                <p><small>Seller {seller}</small></p>
+                <p><small>Ratings {ratings}</small></p>
+            </div>
+            <button className='btnCart'>
+                <p>add to Cart</p>
+            </button>
+
+
         </div>
     );
 };
