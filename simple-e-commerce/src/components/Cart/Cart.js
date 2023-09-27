463761fb7b68f724169e75add8c2ae6329b77a84
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-27 11:25:03 +0600

    grand total calulated

--- simple-e-commerce/src/components/Cart/Cart.js
+++ simple-e-commerce/src/components/Cart/Cart.js
@@ -11,7 +11,8 @@ const Cart = ({carts}) => {
          shipping = shipping +productAll.shipping;
 
     }
-    const tax = (total * 0.1).toFixed(2);
+    const tax = parseFloat((total * 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
     
     return (
         <div className='cart'>
@@ -20,7 +21,7 @@ const Cart = ({carts}) => {
             <p>Total price: {total}</p>
             <p>Total Shipping :{shipping} </p>
             <p>Tax :{tax} </p>
-            <h4>Grand Total : </h4>
+            <h4>Grand Total :{grandTotal.toFixed(2)} </h4>
         </div>
     );
 };
