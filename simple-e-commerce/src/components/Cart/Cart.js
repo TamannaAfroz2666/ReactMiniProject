9d4ed04b08b01d85c9b639331eb7e4798916331b
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-27 10:29:08 +0600

    add position sticky to fixed the menu in top

--- simple-e-commerce/src/components/Cart/Cart.js
+++ simple-e-commerce/src/components/Cart/Cart.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = ({carts}) => {
     return (
-        <div>
+        <div className='cart'>
             <h2>Order Summery</h2>
             <h5>Selected items: {carts.length}</h5>
         </div>
