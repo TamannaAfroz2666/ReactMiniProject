9c9d1bc2a61e0979ae293f27899e1263cc6c8a8d
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-28 00:20:44 +0600

    add Charts working

---
+++ practice-frame/src/components/AssignmentMarks/AssignmentMark.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AssignmentMark = () => {
+    return (
+        <div>
+            <h1>Assignment</h1>
+            
+        </div>
+    );
+};
+
+export default AssignmentMark;
