c5f7885852aa82f7599c0eee1903b12e88207461
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-27 10:55:46 +0600

    add calculations

--- simple-e-commerce/src/components/Cart/Cart.js
+++ simple-e-commerce/src/components/Cart/Cart.js
@@ -2,10 +2,25 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({carts}) => {
+
+    console.log('cart', carts);
+    let total = 0;
+    let shipping = 0
+    for(const productAll of carts){
+         total = total + productAll.price;
+         shipping = shipping +productAll.shipping;
+
+    }
+    const tax = (total * 0.1).toFixed(2);
+    
     return (
         <div className='cart'>
             <h2>Order Summery</h2>
-            <h5>Selected items: {carts.length}</h5>
+            <p>Selected items: {carts.length}</p>
+            <p>Total price: {total}</p>
+            <p>Total Shipping :{shipping} </p>
+            <p>Tax :{tax} </p>
+            <h4>Grand Total : </h4>
         </div>
     );
 };
