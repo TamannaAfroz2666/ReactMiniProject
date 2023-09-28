a5c95a061774a3ecabf65f5e85b4761985e74698
TamannaAfroz2666 <afroztamanna66@gmail.com>
2023-09-28 10:38:46 +0600

    axios called the api

--- practice-frame/src/App.js
+++ practice-frame/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AssignmentMark from './components/AssignmentMarks/AssignmentMark';
+import PhoneBar from './components/PhoneBar/PhoneBar';
 
 function App() {
   return (
     <div>
       <AssignmentMark></AssignmentMark>
+      <PhoneBar></PhoneBar>
      
     </div>
   );
--- practice-frame/src/components/PhoneBar/PhoneBar.js
+++ practice-frame/src/components/PhoneBar/PhoneBar.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const PhoneBar = () => {
+    const [phones, setPhones] = useState([]);
+
+    // data load 
+
+    useEffect(() =>{
+        axios.get(`https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089`).then(data =>{
+            console.log('data is', data);
+        })
+
+    },[])
     return (
         <div>
             <h1>gff</h1>
